c115b6afd4e0089d504d546e1e8db8147869f489
Liangchen <leeonky@gmail.com>
2024-06-28 21:39:25 +0800

    Refactor

---
+++ main/mocker.h
@@ -0,0 +1,101 @@
+#ifndef MOCKER_H
+#define MOCKER_H
+
+#include <stdexcept>
+
+class PageWriteable {
+public:
+    PageWriteable(void *target) {
+        long pageSize = sysconf(_SC_PAGESIZE);
+        pageStart = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(target) & ~(pageSize - 1));
+
+        if (mprotect(pageStart, pageSize, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
+            perror("mprotect");
+            throw std::runtime_error("Change memory writeable failed!");
+        }
+    }
+
+    ~PageWriteable() {
+        long pageSize = sysconf(_SC_PAGESIZE);
+        if (mprotect(pageStart, pageSize, PROT_READ | PROT_EXEC) != 0) {
+            perror("mprotect");
+            exit(-1);
+        }
+    }
+private:
+    void *pageStart;
+};
+
+class Jumper {
+public:
+    virtual ~Jumper() {}
+};
+
+class ShortJumper : public Jumper{
+public:
+    ShortJumper(void *original, void *newAddress): originalAddress(original){
+        intptr_t offset = reinterpret_cast<char*>(newAddress) - reinterpret_cast<char*>(originalAddress) - 2;
+        this->command = *reinterpret_cast<char*>(originalAddress);
+        this->offset_8 = *(reinterpret_cast<char*>(originalAddress) + 1);
+
+        PageWriteable write(originalAddress);
+        *reinterpret_cast<char*>(originalAddress) = 0xEB;
+        *(reinterpret_cast<char*>(originalAddress) + 1) = (unsigned char)offset;
+    }
+    ~ShortJumper() {
+        PageWriteable write(originalAddress);
+        *reinterpret_cast<char*>(originalAddress) = this->command;
+        *(reinterpret_cast<char*>(originalAddress) + 1) = this->offset_8;
+    }
+
+private:
+    void *originalAddress;
+    char command;
+    char offset_8;
+};
+
+class LongJumper : public Jumper{
+public:
+    LongJumper(void *original, void *newAddress): originalAddress(original){
+        intptr_t offset = reinterpret_cast<char*>(newAddress) - reinterpret_cast<char*>(originalAddress) - 5;
+        this->command = *reinterpret_cast<char*>(originalAddress);
+        this->offset = *reinterpret_cast<intptr_t*>(reinterpret_cast<char*>(originalAddress) + 1);
+        PageWriteable write(originalAddress);
+        *reinterpret_cast<char*>(originalAddress) = 0xE9;
+        *reinterpret_cast<intptr_t*>(reinterpret_cast<char*>(originalAddress) + 1) = offset;
+    }
+
+    ~LongJumper() {
+        PageWriteable write(originalAddress);
+        *reinterpret_cast<char*>(originalAddress) = this->command;
+        *reinterpret_cast<intptr_t*>(reinterpret_cast<char*>(originalAddress) + 1) = this->offset;
+    }
+
+    void *originalAddress;
+    char command;
+    intptr_t offset;
+};
+
+class Mocker {
+
+public:
+    Mocker(bool(*original)(), bool(*newFun)()) {
+        void* originalAddress = (void*)(original);
+        void* newAddress = (void*)(newFun);
+        intptr_t offset = reinterpret_cast<char*>(newAddress) - reinterpret_cast<char*>(originalAddress) - 2;
+        if (offset < -128 || offset > 127) {
+            jumper = new LongJumper(originalAddress, newAddress);
+        } else {
+            jumper = new ShortJumper(originalAddress, newAddress);
+        }
+    }
+
+    ~Mocker() {
+        delete jumper;
+    }
+
+private:
+    Jumper *jumper;
+};
+
+#endif
--- test/static_method.cc
+++ test/static_method.cc
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <cstring>
+#include "mocker.h"
 
 class ClassWithStaticMethod {
     public:
@@ -17,84 +18,6 @@ class ClassWithStaticMethod {
 bool return_true() {
     return true;
 }
-
-#include <map>
-#include <vector>
-#include <algorithm>
-
-class Mocker {
-
-public:
-    Mocker(bool(*original)(), bool(*newFun)()) {
-        void* originalAddress = (void*)(original);
-        void* newAddress = (void*)(newFun);
-
-        long pageSize = sysconf(_SC_PAGESIZE);
-        pageStart = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(originalAddress) & ~(pageSize - 1));
-
-        if (mprotect(pageStart, pageSize, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
-            perror("mprotect");
-            exit(-1);
-        }
-
-        intptr_t offset = reinterpret_cast<char*>(newAddress) - reinterpret_cast<char*>(originalAddress) - 2;
-
-        if (offset < -128 || offset > 127) {
-            offset = reinterpret_cast<char*>(newAddress) - reinterpret_cast<char*>(originalAddress) - 5;
-            this->style = 2;
-            this->originalAddress = originalAddress;
-            this->command = *reinterpret_cast<char*>(originalAddress);
-            this->offset = *reinterpret_cast<intptr_t*>(reinterpret_cast<char*>(originalAddress) + 1);
-
-            *reinterpret_cast<char*>(originalAddress) = 0xE9;
-            *reinterpret_cast<intptr_t*>(reinterpret_cast<char*>(originalAddress) + 1) = offset;
-        } else {
-            this->style = 1;
-
-            this->originalAddress = originalAddress;
-            this->command = *reinterpret_cast<char*>(originalAddress);
-            this->offset_8 = *(reinterpret_cast<char*>(originalAddress) + 1);
-
-            *reinterpret_cast<char*>(originalAddress) = 0xEB;
-            *(reinterpret_cast<char*>(originalAddress) + 1) = (unsigned char)offset;
-        }
-
-        if (mprotect(pageStart, pageSize, PROT_READ | PROT_EXEC) != 0) {
-            perror("mprotect");
-            exit(-1);
-        }
-    }
-
-    ~Mocker() {
-        long pageSize = sysconf(_SC_PAGESIZE);
-        if (mprotect(pageStart, pageSize, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
-            perror("mprotect");
-            exit(-1);
-        }
-        switch(this->style) {
-            case 1:
-                *reinterpret_cast<char*>(originalAddress) = this->command;
-                *(reinterpret_cast<char*>(originalAddress) + 1) = this->offset_8;
-                break;
-            default:
-                *reinterpret_cast<char*>(originalAddress) = this->command;
-                *reinterpret_cast<intptr_t*>(reinterpret_cast<char*>(originalAddress) + 1) = this->offset;
-                break;
-        }
-        if (mprotect(pageStart, pageSize, PROT_READ | PROT_EXEC) != 0) {
-            perror("mprotect");
-            exit(-1);
-        }
-    }
-
-private:
-    int style;
-    void *originalAddress;
-    char command;
-    intptr_t offset;
-    char offset_8;
-    void *pageStart;
-};
 
 TEST_GROUP(StaticMethod) {
 };
