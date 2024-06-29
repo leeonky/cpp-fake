414f27fea8885e8e9743d2cf9582633565d80051
Liangchen <leeonky@gmail.com>
2024-06-29 14:16:03 +0800

    more test case

---
+++ test/free_method.cc
@@ -0,0 +1,59 @@
+#include "CppUTest/TestHarness.h"
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include <cstring>
+#include "mocker.h"
+
+bool bool_() {
+    return false;
+}
+
+namespace nm {
+    bool bool_() {
+        return false;
+    }
+}
+
+bool free_return_true() {
+    return true;
+}
+
+TEST_GROUP(FreeMethod) {
+};
+
+TEST(FreeMethod, mock_cxx_global_method) {
+    {
+        Mocker mocker(&bool_, &free_return_true);
+
+        CHECK_TRUE(bool_());
+    }
+
+    CHECK_FALSE(bool_());
+}
+
+TEST(FreeMethod, mock_cxx_namespace_method) {
+    {
+        Mocker mocker(&nm::bool_, &free_return_true);
+
+        CHECK_TRUE(nm::bool_());
+    }
+
+    CHECK_FALSE(nm::bool_());
+}
+
+char *my_strerror(int errnum) {
+    static char buffer[128];
+    sprintf(buffer, "error-is-%d", errnum);
+    return buffer;
+}
+
+TEST(FreeMethod, mock_system_call) {
+    {
+        Mocker mocker(&strerror, &my_strerror);
+
+        STRCMP_EQUAL("error-is-100", strerror(100));
+    }
+
+    STRCMP_EQUAL("Network is down", strerror(100));
+}
--- test/instance_method.cc
+++ test/instance_method.cc
@@ -10,6 +10,35 @@ class ClassWithInstanceMethod {
     bool bool_() {
         return false;
     }
+
+    bool const_bool() const {
+        return false;
+    }
+};
+
+class SubClassWithInstanceMethod : public ClassWithInstanceMethod {
+};
+
+
+class SubDefineA : public ClassWithInstanceMethod {
+    public:
+    bool bool_() {
+        return false;
+    }
+};
+
+class SubDefineB {
+    public:
+    bool bool_() {
+        return false;
+    }
+};
+
+class SubDefineC : public SubDefineA, public SubDefineB {
+    public:
+    bool bool_() {
+        return false;
+    }
 };
 
 bool return_true(ClassWithInstanceMethod *instance) {
@@ -19,7 +48,7 @@ bool return_true(ClassWithInstanceMethod *instance) {
 TEST_GROUP(InstanceMethod) {
 };
 
-TEST(InstanceMethod, non_virtual) {
+TEST(InstanceMethod, mock_non_virtual) {
     ClassWithInstanceMethod instance;
     {
         Mocker mocker(&ClassWithInstanceMethod::bool_, &return_true);
@@ -28,4 +57,143 @@ TEST(InstanceMethod, non_virtual) {
     }
 
     CHECK_FALSE(instance.bool_());
+}
+
+TEST(InstanceMethod, mock_non_virtual_const) {
+    ClassWithInstanceMethod instance;
+    {
+        Mocker mocker(&ClassWithInstanceMethod::const_bool, &return_true);
+
+        CHECK_TRUE(instance.const_bool());
+    }
+
+    CHECK_FALSE(instance.const_bool());
+}
+
+TEST(InstanceMethod, mock_non_virtual_in_sub) {
+    SubClassWithInstanceMethod instance;
+    {
+        Mocker mocker(&ClassWithInstanceMethod::bool_, &return_true);
+
+        CHECK_TRUE(instance.bool_());
+    }
+
+    CHECK_FALSE(instance.bool_());
+}
+
+TEST(InstanceMethod, mock_non_virtual_const_in_sub) {
+    SubClassWithInstanceMethod instance;
+    {
+        Mocker mocker(&ClassWithInstanceMethod::const_bool, &return_true);
+
+        CHECK_TRUE(instance.const_bool());
+    }
+
+    CHECK_FALSE(instance.const_bool());
+}
+
+TEST(InstanceMethod, mock_non_virtual_in_sub_get_address_by_sub_class) {
+    SubClassWithInstanceMethod instance;
+    {
+        Mocker mocker(&SubClassWithInstanceMethod::bool_, &return_true);
+
+        CHECK_TRUE(instance.bool_());
+    }
+
+    CHECK_FALSE(instance.bool_());
+}
+
+TEST(InstanceMethod, mock_non_virtual_const_in_sub_get_address_by_sub_class) {
+    SubClassWithInstanceMethod instance;
+    {
+        Mocker mocker(&SubClassWithInstanceMethod::const_bool, &return_true);
+
+        CHECK_TRUE(instance.const_bool());
+    }
+
+    CHECK_FALSE(instance.const_bool());
+}
+
+TEST(InstanceMethod, mock_non_virtual_redefined_in_sub) {
+    SubDefineA instance;
+    {
+        Mocker mocker(&ClassWithInstanceMethod::bool_, &return_true);
+
+        CHECK_FALSE(instance.bool_());
+
+        CHECK_TRUE(((ClassWithInstanceMethod)instance).bool_());
+    }
+
+    CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+    CHECK_FALSE(instance.bool_());
+
+    {
+        Mocker mocker(&SubDefineA::bool_, &return_true);
+
+        CHECK_TRUE(instance.bool_());
+
+        CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+    }
+
+    CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+    CHECK_FALSE(instance.bool_());
+}
+
+TEST(InstanceMethod, mock_non_virtual_redefined_in_sub_with_multi_base) {
+    SubDefineC instance;
+    {
+        Mocker mocker(&ClassWithInstanceMethod::bool_, &return_true);
+
+        CHECK_FALSE(instance.bool_());
+        CHECK_FALSE(((SubDefineA)instance).bool_());
+        CHECK_FALSE(((SubDefineB)instance).bool_());
+        CHECK_TRUE(((ClassWithInstanceMethod)instance).bool_());
+    }
+
+    CHECK_FALSE(instance.bool_());
+    CHECK_FALSE(((SubDefineA)instance).bool_());
+    CHECK_FALSE(((SubDefineB)instance).bool_());
+    CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+
+    {
+        Mocker mocker(&SubDefineA::bool_, &return_true);
+
+        CHECK_FALSE(instance.bool_());
+        CHECK_TRUE(((SubDefineA)instance).bool_());
+        CHECK_FALSE(((SubDefineB)instance).bool_());
+        CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+    }
+
+    CHECK_FALSE(instance.bool_());
+    CHECK_FALSE(((SubDefineA)instance).bool_());
+    CHECK_FALSE(((SubDefineB)instance).bool_());
+    CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+
+    {
+        Mocker mocker(&SubDefineB::bool_, &return_true);
+
+        CHECK_FALSE(instance.bool_());
+        CHECK_FALSE(((SubDefineA)instance).bool_());
+        CHECK_TRUE(((SubDefineB)instance).bool_());
+        CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+    }
+
+    CHECK_FALSE(instance.bool_());
+    CHECK_FALSE(((SubDefineA)instance).bool_());
+    CHECK_FALSE(((SubDefineB)instance).bool_());
+    CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+
+    {
+        Mocker mocker(&SubDefineC::bool_, &return_true);
+
+        CHECK_TRUE(instance.bool_());
+        CHECK_FALSE(((SubDefineA)instance).bool_());
+        CHECK_FALSE(((SubDefineB)instance).bool_());
+        CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
+    }
+
+    CHECK_FALSE(instance.bool_());
+    CHECK_FALSE(((SubDefineA)instance).bool_());
+    CHECK_FALSE(((SubDefineB)instance).bool_());
+    CHECK_FALSE(((ClassWithInstanceMethod)instance).bool_());
 }
